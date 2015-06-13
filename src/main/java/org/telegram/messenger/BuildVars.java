4c4e040559dafaf100173d2493ecc4444de93a49
cbonet <cbonet@bitbucket.org>
2015-06-13 10:34:05 +0200

    Original Telegram's code with modifications to extract and serialize the Telegram contacts

---
+++ src/main/java/org/telegram/messenger/BuildVars.java
@@ -0,0 +1,19 @@
+/*
+ * This is the source code of Telegram for Android v. 1.3.x.
+ * It is licensed under GNU GPL v. 2 or later.
+ * You should have received a copy of the license in this archive (see LICENSE).
+ *
+ * Copyright Nikolai Kudashov, 2013-2014.
+ */
+
+package org.telegram.messenger;
+
+public class BuildVars {
+    public static boolean DEBUG_VERSION = true;
+    public static int APP_ID = 14274; //obtain your own APP_ID at https://core.telegram.org/api/obtaining_api_id
+    public static String APP_HASH = "1c90450a5cea3c9181251fd0b335d3ac"; //obtain your own APP_HASH at https://core.telegram.org/api/obtaining_api_id
+    public static String HOCKEY_APP_HASH = "a840cf584c406872c8d9266d29d915ceb19305cf";
+    public static String GCM_SENDER_ID = "34670964193";
+    public static String SEND_LOGS_EMAIL = "losuperr@gmail.com";
+    public static String BING_SEARCH_KEY = ""; //obtain your own KEY at https://www.bing.com/dev/en-us/dev-center
+}
